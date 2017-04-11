c70fd686399711188f897c9c5b0afa9ec6ebeb26
Jennifer Oakes <joakes10@gmail.com>
2017-04-11 15:04:55 -0400

    Added ability for panel headers to be clicked and subsequently show or hide, depending on toggle

--- src/repos/panel.directive.js
+++ src/repos/panel.directive.js
@@ -5,15 +5,27 @@
     .directive('panel', PanelDirective);
 
   function PanelDirective() {
+    let $ = angular.element;
+
     return {
       templateUrl: 'repos/panel.template.html',
       restrict: 'E',
+      link: setupCollapse,
       scope: {
         repo: '=repository'
-        // left side: what you can access in the directive's linked above template
+        // left side: what you can access in the directive's linked above template, that is what is on the scope
         // right side: name of an attribute where it is being used
         //if you give me an attribute called 'repository', then I will know where this is
+        //scope of this directive
       }
     };
+
+    function setupCollapse(scope, element) {
+      $(element)
+      .find('header')
+      .on('click', function togglePanelBody() {
+        $(element).find('article').toggleClass('hidden');
+      });
+    }
   }
 }());
