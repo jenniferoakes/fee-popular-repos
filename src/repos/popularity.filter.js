4ba11d6e4f30fa03e1d23f6184dcfa08d6c37df4
Jennifer Oakes <joakes10@gmail.com>
2017-04-12 09:43:25 -0400

    Deleted calculation for popularity because I am doing the work in the service already

--- src/repos/popularity.filter.js
+++ src/repos/popularity.filter.js
@@ -17,9 +17,10 @@
         let collectionCopy = [].concat(collection); //this is a new array that contains all the information from the original array
 
         return collectionCopy.sort(function doPopularityFilter(repo1, repo2) {
-          let popularityRepo1 = ( (repo1.stargazers_count) + (2*(repo1.forks)) + ((1/2)*(repo1.open_issues_count)) );
-          let popularityRepo2 = ( (repo2.stargazers_count) + (2*(repo2.forks)) + ((1/2)*(repo2.open_issues_count)) );
-          return popularityRepo2 - popularityRepo1;
+          // let popularityRepo1 = ( (repo1.stargazers_count) + (2*(repo1.forks)) + ((1/2)*(repo1.open_issues_count)) );
+          // let popularityRepo2 = ( (repo2.stargazers_count) + (2*(repo2.forks)) + ((1/2)*(repo2.open_issues_count)) );
+          // this gets deleted because the popularity value is stored in the service
+          return repo2.popularity - repo1.popularity;
         });
       };
     }
